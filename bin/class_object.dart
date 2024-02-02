3150cd1bd4c1d74adf2b4d425c5acc448657a09a
vishnu300 <vishnukiran300@gmail.com>
2024-02-02 09:19:06 +0530

    intial

---
+++ bin/class_object.dart
@@ -0,0 +1,11 @@
+class Person {
+  String name;
+  int classs;
+
+  Person(this.name, this.classs);
+
+  void displayInfo() {
+    print('Name: $name, classs: $classs');
+  }
+}
+
---
+++ bin/object.dart
@@ -0,0 +1,7 @@
+import 'class_object.dart';
+
+void main() {
+ 
+  Person obj = Person('rahul', 10);
+  obj.displayInfo();
+}
